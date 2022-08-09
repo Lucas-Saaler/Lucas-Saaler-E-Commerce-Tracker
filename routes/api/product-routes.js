0bf0e73cf45a116d03a208b7f86926460c8f62bd
Lucas Saaler <lucassaaler00@gmail.com>
2022-08-09 14:24:06 -0400

    Added GET route for product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,17 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
