a2b81c77fb2ad1f74658bf5ac26a46ec45d30780
Lucas Saaler <lucassaaler00@gmail.com>
2022-08-09 14:19:02 -0400

    Added PUT route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -27,12 +27,27 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.status(200).json({message: "Category updated successfully!"})
+  } catch (err){
+    res.status(400).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
